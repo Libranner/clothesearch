d49f5ca74a015df803bc1d05dc35cb7093acf36f
libranner <libranner@gmail.com>
2016-07-07 16:35:47 -0400

    Initial Commit

---
+++ ClotheSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClotheSearch
+//
+//  Created by Libranner Leonel Santos Espinal on 7/7/16.
+//  Copyright © 2016 Libranner Leonel Santos Espinal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
